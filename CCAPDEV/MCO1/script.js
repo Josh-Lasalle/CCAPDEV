cf7fa266c4a7a931a838aba54ceb4551bc1b2b63
Dane-ai <danielle_kate_sosuntong@dlsu.edu.ph>
2025-10-06 01:22:44 +0800

    Update script.js

--- CCAPDEV/MCO1/script.js
+++ CCAPDEV/MCO1/script.js
@@ -1,11 +1,12 @@
 $(document).ready(function () {
 
-
 //SEARCH 
 $('#searchForm').on('submit', function (e) {
     e.preventDefault();
+    $('#errorMessage').empty();
     clearErrors();
 
+//CONSTANTS
 const errors = [];
 const originCountry = $('#origin').val();
 const destinationCountry = $('#destination').val();
@@ -22,7 +23,29 @@ const countries ={
 
 };
 
-//validation rules
+const flightsDB = [
+  { route: 'manila_beijing', airline: 'BingBong Airline', depart: '05:30', arrive: '08:05', flightNo: 'BB100', price: 303.20 },
+  { route: 'manila_beijing', airline: 'BingBong Airline', depart: '08:00', arrive: '10:35', flightNo: 'BB101', price: 260.20 },
+  { route: 'manila_beijing', airline: 'BingBong Airline', depart: '14:00', arrive: '16:45', flightNo: 'BB102', price: 280.50 },
+
+  { route: 'beijing_manila', airline: 'BingBong Airline', depart: '05:30', arrive: '08:05', flightNo: 'BB200', price: 303.20 },
+  { route: 'beijing_manila', airline: 'BingBong Airline', depart: '08:00', arrive: '10:35', flightNo: 'BB201', price: 260.20 },
+  { route: 'beijing_manila', airline: 'BingBong Airline', depart: '14:00', arrive: '16:45', flightNo: 'BB202', price: 280.50 },
+
+  { route: 'rome_francisco', airline: 'BingBong Airline', depart: '05:30', arrive: '08:05', flightNo: 'BB300', price: 303.20 },
+  { route: 'rome_francisco', airline: 'BingBong Airline', depart: '08:00', arrive: '10:35', flightNo: 'BB301', price: 260.20 },
+  { route: 'rome_francisco', airline: 'BingBong Airline', depart: '14:00', arrive: '16:45', flightNo: 'BB302', price: 280.50 },
+
+  { route: 'francisco_rome', airline: 'BingBong Airline', depart: '05:30', arrive: '08:05', flightNo: 'BB400', price: 303.20 },
+  { route: 'francisco_rome', airline: 'BingBong Airline', depart: '08:00', arrive: '10:35', flightNo: 'BB401', price: 260.20 },
+  { route: 'francisco_rome', airline: 'BingBong Airline', depart: '14:00', arrive: '16:45', flightNo: 'BB402', price: 280.50 },
+
+  { route: 'manila_seoul', airline: 'BingBong Airline', depart: '05:30', arrive: '08:05', flightNo: 'BB110', price: 303.20 },
+  { route: 'manila_seoul', airline: 'BingBong Airline', depart: '08:00', arrive: '10:35', flightNo: 'BB111', price: 260.20 },
+  { route: 'manila_seoul', airline: 'BingBong Airline', depart: '14:00', arrive: '16:45', flightNo: 'BB112', price: 280.50 }
+];
+
+//VALIDATION RULES
 if (!originCountry) {
       errors.push('Please select an origin country.');
       showError('#origin');
@@ -39,17 +62,60 @@ if (originCountry == destinationCountry){
   showError('#destination');
 }
 
-let originText= originCountry;
+if(!departureDate){
+    errors.push('Please select a departure date.');
+    showError('#departureDate');
+}
 
-// Output result
+if(!returnDate){
+    errors.push('Please select a return date.');
+    showError('#returnDate');
+}
+
+if(!passengerCount){
+    errors.push('Please fill in this field.');
+    showError('#passengerCount');
+}
+
+let originText = countries[originCountry];
+let destinationText = countries[destinationCountry];
+
+//OUTPUT RESULT
     if (errors.length > 0) { // checks if errors array has elements
-      $('#errorMessage').html(` <div class="alert alert-danger ">Errors: <br> ${errors.join('<br>')}</div>`);
+      $('#errorMessage').html(`<div class="alert alert-danger "><b>Error</b> <br> ${errors.join('<br>')}</div>`);
     } else {
-      let html = '<div class="main_wrap"> <h6> Departing flight</h6> <br> <h3> <div class="originText"></div> to <div class="destinationText"></div>  |  </h3> <a href="search.html">Modify Search</a> </div>';
-      $('#flightMessage').html(html);
+      let flightMessage = `<div class="customBorder">
+                            <div class="main_wrap"> 
+                              <h4> ${originText} to 
+                              ${destinationText}  |  
+                              </h4><a href="search.html">Modify Search</a> 
+                            </div> 
+                          
+
+                            <table class="table table-striped-columns m-u">
+                            
+                              <tr>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                                <td></td>
+                              </tr>
+                          
+                              <tr>
+                                <td>BingBong Airline</td>
+                                <td>08:00 - 10:35</td>
+                                <td>CX912</td>
+                                <td>USD260.20</td>
+                              </tr>
+
+                            </table>
+                          </div>
+                          `;
+      $('#flightMessage').html(flightMessage);
     }
   });
 
+//ERROR
 function showError(selector) { // takes the id and changes the element into a red border color
     $(selector).addClass('is-invalid');
   }
