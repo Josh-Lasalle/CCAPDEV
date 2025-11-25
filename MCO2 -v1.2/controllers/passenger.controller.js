a5d5f5b238a0e9b2c36a06be48e26b0383dab266
Dane-ai <danielle_kate_sosuntong@dlsu.edu.ph>
2025-11-25 21:00:49 +0800

    Delete MCO2 -v1.2 directory

--- MCO2 -v1.2/controllers/passenger.controller.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const Passenger = require('../models/passenger.model')
-
-module.exports = router
--- MCO2 -v1.2/controllers/seat.controller.js
+++
@@ -1,6 +0,0 @@
-const express = require('express')
-const router = express.Router()
-
-const Seat = require('../models/seat.model')
-
-module.exports = router
