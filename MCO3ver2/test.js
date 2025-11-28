cc3d0ff2580889b27cb609118948fc4028e9f469
Dane-ai <danielle_kate_sosuntong@dlsu.edu.ph>
2025-11-28 22:40:55 +0800

    Update test.js

--- MCO3ver2/test.js
+++ MCO3ver2/test.js
@@ -4,43 +4,35 @@ const User = require('./models/user.model');
 const Flight = require('./models/flight.model');
 const Passenger = require('./models/passenger.model');
 const Url = 'http://localhost:3000';
-const agent = superT.agent(Url);
+const client = superT.agent(Url); 
+const admin = superT.agent(Url);
 
 beforeAll(async () => {
     await mongoose.connect('mongodb://127.0.0.1:27017/airlinedb');
+    await User.create({ username: 'testAdmin', password: '12345678', email: 'testAdmin@gmail.com', role: 'Admin' });
+    await admin.post('/users/login').send({ username: 'testAdmin', password: '12345678' });
 });
 
 afterAll(async () => {
     await User.deleteMany({
-        username: {$in: ['testClient', 'testAdmin', 'updatedTestClient']}
+        username: {$in: ['testClient', 'testAdmin']}
     });
     await mongoose.connection.close();
 });
 
 describe('Create test users', () => {
     it('Should register client users', async () => {
-        const res = await agent
-            .post('/users/register')
-            .send({
+        const res = await client.post('/users/register').send({
                 username: 'testClient',
                 password: '12345678',
-                email: 'testClient@gmail.com'
+                email: 'testClient@gmail.com',
+                role: 'Client'
             });
         expect(res.statusCode).toBe(302)
         expect(res.headers.location).toBe('/users/login');
-    });
 
-    it('Should add admin users', async () => {
-        const res = await agent
-            .post('/users/add')
-            .send({
-                username: 'testAdmin',
-                password: '12345678',
-                email: 'testAdmin@gmail.com',
-                role: 'Admin'
-            });
-        expect(res.statusCode).toBe(302)
-        expect(res.headers.location).toBe('/users');
+        const clientCheck = await User.findOne({username: 'testClient'});
+        expect(clientCheck).not.toBeNull();
     });
 
 });
@@ -50,38 +42,15 @@ describe('Login', () => {
 
         const clientUser = await User.findOne({username: 'testClient'});
 
-        const res = await agent 
-        .post('/users/login')
-        .send({username: clientUser.username, password: '12345678'});
+        const res = await client.post('/users/login').send({username: clientUser.username, password: '12345678'});
 
         expect(res.statusCode).toBe(302); 
         expect(res.headers.location).toBe('/client/home'); 
     });
 
-    test('Admin user should login and be redirected to Admin Page', async () => {
-
-        const AdminUser = await User.findOne({username: 'testAdmin'});
-
-        const res = await agent 
-        .post('/users/login')
-        .send({username: AdminUser.username, password: '12345678'});
-
-        expect(res.statusCode).toBe(302); 
-        expect(res.headers.location).toBe('/admin/home'); 
-    });
 });
 
 describe('Client Profile Update', () => {
-
-    beforeAll(async () => {
-        const clientUser = await User.findOne({username: 'testClient'});
-        const res = await agent
-            .post('/users/login')
-            .send({username: clientUser.username, password: '12345678'});
-
-        expect(res.statusCode).toBe(302); 
-        expect(res.headers.location).toBe('/client/home'); 
-    });
 
     it('Should update client username & email successfully', async () => {
         const updatedData = {
@@ -91,9 +60,7 @@ describe('Client Profile Update', () => {
             confirmPassword: ''
         };
 
-        const res = await agent
-            .post('/users/profile/update')
-            .send(updatedData);
+        const res = await client.post('/users/profile/update').send(updatedData);
                 
             expect(res.statusCode).toBe(302);
             expect(res.headers.location).toBe('/client/profile?status=1');
@@ -107,15 +74,44 @@ describe('Client Profile Update', () => {
             confirmPassword: 'test1234'
         };
 
-        const res = await agent
-            .post('/users/profile/update')
-            .send(updatedData);
+        const res = await client.post('/users/profile/update').send(updatedData);
                 
             expect(res.statusCode).toBe(302);
             expect(res.headers.location).toBe('/client/profile?status=1');
 
     });
 
+    test('Fails as password do not match', async () => {
+        const updatedData = {
+            username: 'updatedTestClient',
+            email: 'updatedTestClient@gmail.com',
+            password: 'test1234',
+            confirmPassword: 'failtest1234'
+        };
+
+        const res = await client
+            .post('/users/profile/update')
+            .send(updatedData);
+                
+            expect(res.statusCode).toBe(302);
+            expect(res.headers.location).toBe('/client/profile?error=2');
+    });
+
+    test('Fails as username is invalid', async () => {
+        const updatedData = {
+            username: '',
+            email: 'updatedTestClient@gmail.com',
+            password: 'test1234',
+            confirmPassword: 'test1234'
+        };
+
+        const res = await client.post('/users/profile/update').send(updatedData);
+                
+            expect(res.statusCode).toBe(302);
+            expect(res.headers.location).toBe('/client/profile?error=3');
+    });
+
+    // force revert
     afterAll(async () => {
         const clientUser = await User.findOne({username: 'updatedTestClient'});
 
@@ -129,17 +125,7 @@ describe('Client Profile Update', () => {
 });
 
 describe('Flight Creation', () => {
-
-    beforeAll(async () => {
-        const adminUser = await User.findOne({username: 'testAdmin'});
-        const res = await agent
-            .post('/users/login')
-            .send({username: adminUser.username, password: '12345678'});
-
-        expect(res.statusCode).toBe(302); 
-        expect(res.headers.location).toBe('/admin/home'); 
-    });
-
+  
     test('Admin should be able to create flights', async () => {
 
         const flightData =  {
@@ -156,9 +142,7 @@ describe('Flight Creation', () => {
             price: 2500,
         };
 
-        const res = await agent
-            .post('/flights/add')
-            .send(flightData);
+        const res = await admin.post('/flights/add').send(flightData);
         
         expect(res.statusCode).toBe(302);
         expect(res.headers.location).toBe('/flights');
@@ -168,6 +152,27 @@ describe('Flight Creation', () => {
 
     });
 
+    test('Failed as flight number is invalid', async () => {
+
+        const flightData =  {
+            flightNum: 'A1',
+            airline: 'Bing Bong Airlines',
+            aircraftType: 'Bing Bong Carrier',
+            origin: 'Manila',
+            destination: 'Beijing',
+            departureDate: '2025-12-30',
+            departureTime: '08:00',
+            arrivalDate: '2025-12-30',
+            arrivalTime: '10:00',
+            seatCap: 60,
+            price: 2500,
+        };
+
+        const res = await admin.post('/flights/add').send(flightData);
+        
+        expect(res.statusCode).toBe(200);
+        expect(res.text).toContain('Flight number must be exactly 5 characters long.');
+    });
 });
 
 
@@ -176,9 +181,7 @@ describe('Client Booking', () => {
 
     test('Client can render book flight', async () => {
         const flight = await Flight.findOne({flightNum: 'BB123'});
-        const res = await agent
-            .post('/client/bookFlight')
-            .send({
+        const res = await client.post('/client/bookFlight').send({
                 origin: flight.origin,
                 destination: flight.destination,
                 departure: "2025-12-10",
@@ -192,9 +195,7 @@ describe('Client Booking', () => {
     
     test('Client can register to a flight', async () => {
         const flight = await Flight.findOne({flightNum: 'BB123'});
-        const res = await agent
-            .post('/client/register')
-            .send({
+        const res = await client.post('/client/register').send({
                 flightId: flight._id,
                 passengerCount: 1
             });
@@ -208,9 +209,7 @@ describe('Client Booking', () => {
         const flight = await Flight.findOne({flightNum: 'BB123'});
         const clientUser = await User.findOne({username: 'testClient'});
 
-        const res = await agent
-            .post('/client/submitReservation')
-            .send({
+        const res = await client.post('/client/submitReservation').send({
                 fullName: clientUser.username,
                 email: clientUser.email,
                 passportID: 'A1234567',
@@ -226,10 +225,25 @@ describe('Client Booking', () => {
 
     });
 
+    // client can delete booking reservation
+
     afterAll(async () => {
         const flight = await Flight.findOne({flightNum: 'BB123'});
         await Flight.deleteOne({ _id: flight._id });
         await Passenger.deleteOne({ flightNum: flight.flightNum });
+    });   
+});
+
+describe('Deleting Users', () => {
+    let deleteUser;
+
+    test('Delete the test client user as Admin', async () => {
+        deleteUser = await User.findOne({username: 'testClient'});
+
+        const res = await admin.post(`/users/delete/${deleteUser._id}`);
+
+        expect(res.statusCode).toBe(302);
+        expect(res.headers.location).toBe('/users');
     });
-    
+   
 });
